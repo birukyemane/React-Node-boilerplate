827073574ea2c2c5d9d70c052f19b7da6199ca32
birukyemane <bir_yem@yahoo.com>
2019-01-31 21:53:29 +0200

    minor changes

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const setAuthToken = token => {
+const setAuthToken = token => {  
   if (token) {
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common['Authorization'] = token;// set token in header if there is token
   } else {
     axios.defaults.headers.common['Authorization'] = null;
   }
